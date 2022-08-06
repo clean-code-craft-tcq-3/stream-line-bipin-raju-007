d1d1ee7ce11d271678d52bb91911db37fcaf8669
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-06 11:50:37 +0530

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -3,7 +3,7 @@
 
 void readDataFromConsole(int* batteryCharge, int* batteryTemperature);
 void maxAndMinValue(int *sensorparameter, int *max, int *min);
-bool checkAvgWithinRange(float *movingAvg, float limit);
+bool checkAvgWithinRange(float movingAvg, float limit);
 float calculateSimpleMovingAverage(int *sensorparameter);
-void printRxDataAndAvgToConsole(int *RxData, float SMA);
+void printRxDataAndAvgToConsole(int *RxData, float SMA, int maxVal, int minVal);
 bool receiveAndProcessSensorData(int* batteryCharge, int* batteryTemperature, float chargeLimit, float tempLimit);
