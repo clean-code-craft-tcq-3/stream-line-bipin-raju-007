b3d8c8eb1bd38a6368543f1905ea1e57284d6928
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-06 12:10:27 +0530

    Update testReceiver.cpp

--- testReceiver.cpp
+++ testReceiver.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Test to check whether sensor data parameters are in Range")
 {
   int batteryCharge[50] = {0};
   int batteryTemperature[50] = {0};
-  float chargelimit = 99.0, templimit = 50.0
+  float chargeLimit = 99.0, tempLimit = 50.0;
   bool isParametersInRange;
   isParametersInRange = receiveAndProcessSensorData(&batteryCharge[0],&batteryTemperature[0], chargeLimit, tempLimit);
   REQUIRE(isParametersInRange == 1);
